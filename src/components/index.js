c540e531c0cfc94aecef1fa2a0c385228da78d10
Alexis <pingrisalexis@gmail.com>
2022-03-01 13:18:31 +0100

    Add Banner

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Header } from './header/Header';
+export { default as Banner } from './banner/Banner';
 
 export { default as LodgingList } from './lodging-list/LodgingList';
-export { default as LodgingCard } from './lodging-list/LodgingCard';
+// export { default as LodgingCard } from './lodging-list/LodgingCard';
 export { default as Lodging } from './lodging-list/Lodging';
