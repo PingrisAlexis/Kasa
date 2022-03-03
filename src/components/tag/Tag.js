c5749bff75ff01e5c447229c5d7fd31d007b2c3d
Alexis <pingrisalexis@gmail.com>
2022-03-03 18:45:35 +0100

    create tag

---
+++ src/components/tag/Tag.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from "./Tag.module.scss";
+
+export default class Accordion extends  Component {
+    render() {
+        const {tags} = this.props
+        return (
+             <ul className={styles.tags_container}>
+                 {tags.map((elt, index) =>
+                 <li key={index}>{elt}</li>)}
+             </ul>
+        )
+    }
+}
