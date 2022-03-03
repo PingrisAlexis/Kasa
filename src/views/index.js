f67511d9e84cac8b303788350a28b1b37158721a
Alexis <pingrisalexis@gmail.com>
2022-03-03 18:45:59 +0100

    add lodging page

--- src/views/index.js
+++ src/views/index.js
@@ -1,2 +1,3 @@
 export { default as HomePage } from './home-page/HomePage';
 export { default as AboutPage } from './about-page/AboutPage';
+export { default as LodgingPage } from './lodging-page/LodgingPage';
