173cd91a34cee92520be7fc7f0415d1ef80c8b6f
Alexis <pingrisalexis@gmail.com>
2022-03-03 18:45:16 +0100

    add navlink

--- src/components/home-lodging-card/LodgingCard.js
+++ src/components/home-lodging-card/LodgingCard.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import styles from "./LodgingCard.module.scss";
-import { Link } from "react-router-dom";
+import { NavLink  } from "react-router-dom";
 
 export default class LodgingCard extends Component {
     render() {
         const { lodging } = this.props;
         return (
-            <Link className={styles.lodging_card} to={{pathname: `/lodging/${lodging.id}`}} key={lodging.id}>
+            <NavLink  className={styles.lodging_card} to={`/lodging/${lodging.id}`} key={lodging.id}>
                 <div>
                     <img src={lodging.cover} alt="" />
                     <h2>{lodging.title}</h2>
                 </div>
-             </Link>
+             </NavLink>
         );
     }
 }
