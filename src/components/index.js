e34133aeda7a7b3c013ea0bc1f960822cdd967cf
Alexis <pingrisalexis@gmail.com>
2022-03-03 18:45:26 +0100

    refactoring

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,8 @@
 export { default as Header } from './header/Header';
 export { default as HomeBanner } from './home-banner/HomeBanner';
-export { default as HomeContent } from './home-content/HomeContent';
 export { default as LodgingCard } from './home-lodging-card/LodgingCard';
 export { default as AboutBanner } from './about-banner/AboutBanner';
-export { default as AboutContent } from './about-content/AboutContent';
 export { default as Footer } from './footer/Footer';
 export { default as NotFound } from './not-found/NotFound';
 export { default as Accordion } from './accordion/Accordion';
+export { default as Tag } from './tag/Tag';
