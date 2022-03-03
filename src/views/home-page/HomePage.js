ad65077a5ac5fcfc0ccb26e739cdf275823e2818
Alexis <pingrisalexis@gmail.com>
2022-03-03 18:46:14 +0100

    refactoring home page

--- src/views/home-page/HomePage.js
+++ src/views/home-page/HomePage.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import { HomeContent, HomeBanner } from '../../components';
+import { HomeBanner, LodgingCard} from '../../components';
+import styles from "./HomePage.module.scss";
+import {data} from "../../datas/data";
 
 export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            lodgings: data,
+        };
+    }
     render() {
         return (
                 <main>
                     <HomeBanner />
-                    <HomeContent />
+
+                    <div className={styles.lodgings_contenair}>
+                        <div className={styles.lodgings_list}>
+                            {this.state.lodgings.map((lodging) => {
+                                return (
+                                    <LodgingCard key={lodging.id} lodging={{ ...lodging }} />
+                                );
+                            })}
+                        </div>
+                    </div>
+
                 </main>
         );
     }
