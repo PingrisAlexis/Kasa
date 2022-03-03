25b9ca232d16d2c9046dfe1f8e5c88fe8df9c485
Alexis <pingrisalexis@gmail.com>
2022-03-03 18:45:05 +0100

    add accordion

--- src/views/about-page/AboutPage.js
+++ src/views/about-page/AboutPage.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
-import { AboutBanner } from '../../components';
-import { AboutContent } from '../../components';
+import {AboutBanner, Accordion} from '../../components';
+import styles from "./AboutPage.module.scss";
+import {aboutData} from "../../datas/aboutData";
 
 export default class AboutPage extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            aboutMessages: aboutData,
+        };
+    }
     render() {
         return (
                 <main>
                     <AboutBanner />
-                    <AboutContent />
+                    <div className={styles.accordions_container}>
+                        {this.state.aboutMessages.map((aboutMessage) => {
+                            return (
+                                <Accordion key={aboutMessage.id} accordionWidth={styles.accordion_width} title={aboutMessage.title} content={aboutMessage.description} />
+                            );
+                        })}
+                    </div>
                 </main>
         );
     }
