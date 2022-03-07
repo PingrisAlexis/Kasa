2191b3ced0768005185a10af2e7fec395dd24fe3
Alexis <pingrisalexis@gmail.com>
2022-03-07 20:32:15 +0100

    Creates Rating component

---
+++ src/components/rating/Rating.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import styles from "./Rating.module.scss";
+import { BsStarFill } from 'react-icons/bs'
+
+export default class Rating extends  Component {
+    render() {
+        const {rating} = this.props;
+        let lodgingRating = rating;
+        const ratingScale = [];
+        const maxRating = 5;
+
+        for (let i = 0; i < maxRating; i++) {
+            lodgingRating > 0 ? ratingScale.push(<BsStarFill className={styles.rating_full} />) : ratingScale.push(<BsStarFill className={styles.rating_empty} />)
+            lodgingRating--;
+        }
+        
+        return (
+            <div className={styles.rating_container}>
+                {
+                    ratingScale.map((elt, index) => (
+                        <span key={index}>{elt}</span>
+                    ))
+                }
+            </div>
+
+        )
+    }
+}
