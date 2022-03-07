57daf2c977ff0dfac620cef7ae40d17bd4bff64a
Alexis <pingrisalexis@gmail.com>
2022-03-07 20:31:42 +0100

    Create Host component

---
+++ src/components/host/Host.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from "./Host.module.scss";
+
+export default class Host extends  Component {
+    render() {
+        const {name, picture} = this.props;
+        return (
+            <figure className={styles.host_container}>
+                <figcaption>
+                    <p>{name.split(" ")[0]}</p>
+                    <p>{name.split(" ")[1]}</p>
+                </figcaption>
+                <img src={picture} alt={`${name}'s picture`}/>
+            </figure>
+        )
+    }
+}
