42c1bb5f10d33a75ba5da440265849667e7432da
Alexis <pingrisalexis@gmail.com>
2022-03-07 20:31:29 +0100

    Add ALT

--- src/components/home-lodging-card/LodgingCard.js
+++ src/components/home-lodging-card/LodgingCard.js
@@ -8,7 +8,7 @@ export default class LodgingCard extends Component {
         return (
             <NavLink  className={styles.lodging_card} to={`/lodging/${lodging.id}`} key={lodging.id}>
                 <div>
-                    <img src={lodging.cover} alt="" />
+                    <img src={lodging.cover} alt={`${lodging.title}'s picture`} />
                     <h2>{lodging.title}</h2>
                 </div>
              </NavLink>
